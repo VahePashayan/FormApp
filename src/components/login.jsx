2b04a0e341c8fb9a7e4e0014ea5bd1b032ba4c7f
VahePashayan <vpashayan39gmail.com>
2021-07-19 02:04:03 +0400

    feat: created a input.jsx file for input component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,49 +1,42 @@
 import React, { Component } from 'react';
+import Input from './input';
 
 class LoginForm extends Component {
   state = {
-    account: { username: '', password: '' },
+    account: { username: '', password: '' }
   };
+
+
 
   handleSubmit = (e) => {
     e.preventDefault();
   };
 
   handleChange = (e) => {
-    const account = {...this.state.account};
-    account[e.currentTarget.name]  = e.currentTarget.value;
-    this.setState({account});
-  }
+    const account = { ...this.state.account };
+    account[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ account });
+  };
 
   render() {
-
-    const {account} = this.state;
+    const { account } = this.state;
 
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor="username">Username</label>
-            <input
-              value={account.username}
-              onChange={this.handleChange}
-              autoFocus
-              id="username"
-              name="username"
-              type="text"
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              id="password"
-              name="password"
-              type="text"
-            />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
           <button>Login</button>
         </form>
       </div>
