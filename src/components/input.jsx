2084522a23d69add88c690acb4a38cb879b6ffa4
VahePashayan <vpashayan39gmail.com>
2021-07-21 17:04:07 +0400

    feat: added the validation of forms

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const Input = ({name, label, value, onChange}) => {
+const Input = ({name, label, value, onChange, type}) => {
   return (
     <div>
       <label htmlFor={name}>{label}</label>
       <input
         value={value}
         onChange={onChange}
-        autoFocus
         id={name}
         name={name}
-        type="text"
+        type={type}
       />
     </div>
   );
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import Input from './input';
-
+import "./login.css"
 class LoginForm extends Component {
   state = {
-    account: { username: '', password: '' }
+    account: { username: '', password: '' },
+    isUsernameFilled: false,
+    isPasswordFilled: false
   };
-
-
 
   handleSubmit = (e) => {
     e.preventDefault();
+    if(this.state.account.username)
+    {
+      this.setState(() => ({ isUsernameFilled: true}));
+    }else {
+      this.setState(() => ({ isUsernameFilled: false}));
+    }
+    if(this.state.account.password)
+    {
+      this.setState(() => ({ isPasswordFilled: true}));
+    }else {
+      this.setState(() => ({ isPasswordFilled: false}));
+    }
   };
 
   handleChange = (e) => {
@@ -30,13 +42,17 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            type="text"
           />
+          <p className={this.state.isUsernameFilled ? "hidden" : "visible"} style={{color: "red"}}>username is required</p>
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            type="password"
           />
+          <p className={this.state.isPasswordFilled ? "hidden" : "visible"} style={{color: "red"}}>password is required</p>}
           <button>Login</button>
         </form>
       </div>
